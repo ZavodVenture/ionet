870ec3efdf35d2a95bf5f23d63407c14586c3c09
ZavodVenture <127696238+ZavodVenture@users.noreply.github.com>
2024-03-28 13:50:02 +0300

    Update reloader.py

--- reloader.py
+++ reloader.py
@@ -1,6 +1,7 @@
 import argparse
 from os import system
 from time import sleep
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('device_id')
@@ -10,17 +11,69 @@ parser.add_argument('delay')
 args = parser.parse_args()
 
 
+def get_token():
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14dGZka3BweHlmbG1tZ2x1bGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwNDI1ODEsImV4cCI6MjAyMzYxODU4MX0.mNkDiJaCBB5twRNypzThEKl-s8d5VjasNyJj1l9BK9o'
+    refresh_token = open('refresh_token').read()
+
+    try:
+        r = requests.post('https://id.io.net/auth/v1/token?grant_type=refresh_token',
+                          json={'refresh_token': refresh_token},
+                          headers={'Apikey': token,
+                                   'Authorization': f'Bearer {token}'})
+    except Exception as e:
+        raise Exception('Ошибка во время получения токена')
+    else:
+        r = r.json()
+
+        if 'error' in r:
+            raise Exception(r['error_description'])
+
+        with open('refresh_token', 'w') as file:
+            file.write(r['refresh_token'])
+            file.close()
+
+        return r['access_token']
+
+
+def get_node_status(token):
+
+    try:
+        r = requests.get(f'https://production.io.systems/v1/io-worker/users/{args.user_id}/devices',
+                         params={'page': 1,
+                                 'page_size': 20},
+                         headers={'Token': token})
+    except Exception as e:
+        raise Exception('Ошибка во время получения статуса ноды')
+    else:
+        r = r.json()
+
+        if 'status' in r and r['status'] == 'succeed':
+            return r['devices'][0]['status']
+        else:
+            raise Exception('Ошибка во время получения статуса ноды')
+
+
 def reload():
-    command = f'./launch_binary_linux --device_id={args.device_id} --user_id={args.user_id} --operating_system="Linux" --usegpus=false --device_name={args.device_name}'
-    system(command)
-    sleep(2)
-    system(command)
+    status = get_node_status(get_token())
+
+    if status == 'down':
+        system('sudo docker stop $(sudo docker ps -a -q); sudo docker rm $(sudo docker ps -a -q) && sudo docker rmi $(sudo docker images -a -q) -f')
+
+        command = f'./launch_binary_linux --device_id={args.device_id} --user_id={args.user_id} --operating_system="Linux" --usegpus=false --device_name={args.device_name}'
+        system(command)
+        system(command)
+    else:
+        print(f'restart not needed: {status}')
 
 
 def worker():
     while 1:
         sleep(int(args.delay))
-        reload()
+
+        try:
+            reload()
+        except Exception as e:
+            print(f'Ошибка: {e}')
 
 
 if __name__ == '__main__':
